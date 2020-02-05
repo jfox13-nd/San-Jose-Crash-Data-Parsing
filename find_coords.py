dc93c4e218f35e1b274fedb6401db59038e66008
Jack Fox <jfox13@nd.edu>
2020-02-05 13:14:01 -0800

    fixes to find_coords

--- find_coords.py
+++ find_coords.py
@@ -14,11 +14,15 @@ import re
 import certifi
 import urllib
 import math
+import pprint
 
 r39_json_name = "r39.json"
 r39_json_name_coords = "r39_coords.json"
 # radius of earth in km
 radius_earth = 6371.0
+# feet
+gps_accuracy = 50
+api_call_limit = 5
 
 with open('.secret','r') as secret_file:
     API_KEY = secret_file.readlines()[0].strip()
@@ -47,15 +51,17 @@ def find_gps_coordinates(collision: dict) -> tuple:
     * If no distance or direction is available then the intersection position is returned
     * If no direction is available then the intersection point is returned
     * If the location of an intersection cannot be found then (None, None) is returned
+    * If there is a failure to snap a point to a nearby road then (None, None) is returned
     '''
     addr = collision["IntersectionName"]
     distance = collision["Distance"]
 
     # convert feet to km
     if distance:
-        distance *= 0.3048
+        distance_km *= 0.3048
 
     intersection_res = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(addr,API_KEY))
+    intersection_res = intersection_res.json
     if not intersection_res['results'] or not intersection_res['results'][0] or not intersection_res['results'][0]['geometry'] or not intersection_res['results'][0]['geometry']['location']:
         print("Failed to get gps coordinates of crash at {}".format(addr))
         return None,None
@@ -65,15 +71,17 @@ def find_gps_coordinates(collision: dict) -> tuple:
 
     if not distance:
         return intersection_lat, intersection_lng
-    real_lat = intersection_lat
-    real_lng = intersection_lng
-
+    prev_lat = intersection_lat
+    prev_lng = intersection_lng
+    current_lat = lat_adjust(prev_lat, distance_km)
+    current_lng = lat_adjust(prev_lat, prev_lng, distance_km)
     
-    
-    return real_lat,real_lng
+    snap_point(current_lat,current_lng)
 
-def uo(args, **kwargs):
-    return urllib.request.urlopen(args, cafile=certifi.where(), **kwargs)
+    if not current_lat and not current_lng:
+        return None, None
+
+    return current_lat, current_lng
 
 def lat_adjust(lat: float, km: float) -> float:
     ''' returns adjusted lattitude, positive km argument for North, negative for South '''
@@ -82,6 +90,27 @@ def lat_adjust(lat: float, km: float) -> float:
 def lng_adjust(lat: float, lng: float, km: float) -> float:
     ''' returns adjusted lattitude, positive km argument for East, negative for West '''
     return lng + (km / radius_earth) * (180.0 / math.pi) / math.cos(lat * math.pi / 180.0)
+
+def feet_to_km(feet: float) -> float:
+    return feet * 0.3048
+
+def km_to_feet(km: float) -> float:
+    return km / 0.3048
+
+def distance_between(lat1: float, lng1: float, lat2: float, lng2: float) -> float:
+    ''' calculates distance between two gps coordinates in feet '''
+    return geopy.distance.distance( (lat1,lng1), (lat2,lng2) ).feet
+
+def snap_point(lat: float, lng: float) -> tuple:
+    ''' snap a gps point to the nearest road, find that gps point '''
+    res = requests.get('https://roads.googleapis.com/v1/snapToRoads?path={},{}&key={}'.format(lat,lng,API_KEY))
+    res = res.json
+    try:
+        return res['snappedPoints'][0]['location']['latitude'], res['snappedPoints'][0]['location']['longitude']
+    except:
+        print("Snap failed:")
+        pprint.pprint(res)
+        return None,None
 
 if __name__ == '__main__':
     generate_coords_file()
